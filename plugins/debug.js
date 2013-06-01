8dd83e5fe0ea60d7124175bf32ccc7065edef6a9
Jean-Sébastien Tremblay <jeansebtr@xpensia.com>
2013-06-01 15:04:28 -0400

    implement STARTTLS

--- plugins/debug.js
+++ plugins/debug.js
@@ -1,26 +1,46 @@
 
-var plugin = module.exports;
+var Writable = require('stream').Writable;
 
-plugin.connection = function(connection, next) {
-    var stream = connection.stream;
-    console.log('[%s:%d] (Connected)', stream.remoteAddress, stream.remotePort);
-    stream.on('data', onData.bind({}, stream));
-    next();
-};
+var plugin = module.exports = {};
 
-function onData(stream, data) {
-    var lines = data.toString('utf8').split('\n');
+function LogStream(addr, port) {
+    Writable.call(this);
+
+    this.addr = addr;
+    this.port = port;
+    this.last = null;
+}
+LogStream.prototype = Object.create(Writable.prototype);
+LogStream.prototype._write = function(chunk, encoding, cb) {
+    var lines = chunk.toString('utf8').split('\n');
     if(this.last) {
         lines[0] = this.last+lines[0];
     }
     for(var i=0; i<lines.length-1; i++) {
-        console.log('[%s:%d] >>> %s', stream.remoteAddress, stream.remotePort, lines[i].trim());
+        console.log('[%s:%d] >>> %s', this.addr, this.port, lines[i].trim());
     }
     this.last = lines[i].trim();
-}
+    cb();
+};
 
-plugin.unknown_command = function(connection, line, next) {
-    console.log('[%s:%d] Unknown %s :', connection.notes.remoteAddress,
-        connection.notes.remotePort, line.command, line.args);
+plugin.connection = function(c, next) {
+    var stream = c.stream;
+    var log = new LogStream(c.notes.remoteAddress, c.notes.remotePort);
+    console.log('[%s:%d] (Connected)', c.notes.remoteAddress, c.notes.remotePort);
+    stream.pipe(log);
+    next();
+};
+
+plugin.secure = function(c, next) {
+    var stream = c.stream;
+    var log = new LogStream(c.notes.remoteAddress, c.notes.remotePort);
+    console.log('[%s:%d] (Secured)', c.notes.remoteAddress, c.notes.remotePort);
+    stream.pipe(log);
+    next();
+};
+
+plugin.unknown_command = function(c, line, next) {
+    console.log('[%s:%d] Unknown %s :', c.notes.remoteAddress,
+        c.notes.remotePort, line.command, line.args);
     next(null, 'BAD', 'Unknown command');
 };
--- plugins/starttls.js
+++ plugins/starttls.js
@@ -1,10 +1,78 @@
 
+var tls = require('tls');
+var crypto = require('crypto');
+var emit = require('events').EventEmitter.prototype.emit;
+
 var plugin = module.exports;
 
-plugin.getCapabilities = function(connection) {
-    return ['STARTTLS'];
+plugin.initialize = function(opts) {
+    if(!opts || !opts.key || !opts.cert) {
+        throw new Error("Can't enable STARTTLS plugin: missing certs configuration");
+    }
+    else {
+        //opts.ciphers = 'ALL';
+        this.notes._credentials = crypto.createCredentials(opts);
+    }
 };
 
-plugin.starttls = function(connection) {
-    
+plugin.getCapabilities = function(connection) {
+    if(this.notes._credentials) {
+        return ['STARTTLS'];
+    }
+    else {
+        return [];
+    }
 };
+
+var events = ['error', 'close'];
+
+plugin.starttls = function(connection, tag) {
+    if(!this.notes._credentials) {
+        throw new Error('No credentials available');
+    }
+    connection.pause();
+
+    var socket = connection.stream;
+    socket.ondata = null;
+    socket.unpipe();
+    var securePair = tls.createSecurePair(this.notes._credentials, true, false, false);
+    var clearText = securePair.cleartext;
+
+    for(var i=0; i<events.length; i++) {
+        var event = events[i];
+        var listeners = (socket.listeners(event) || []).slice(0);
+        socket.removeAllListeners(event);
+        for(var j=0; j<listeners.length; j++) {
+            clearText.on(event, listeners[j]);
+            socket.on(event, emit.bind(clearText, event));
+        }
+    }
+
+    securePair.fd = socket.fd;
+    clearText.socket = socket;
+    clearText.encrypted = securePair.encrypted;
+    clearText.authorized = false;
+
+    securePair.on('error', emit.bind(clearText, 'error'));
+    securePair.on('secure', function() {
+        var verifyError = (securePair.ssl || securePair._ssl).verifyError();
+
+        // A cleartext stream has the boolean property 'authorized' to determine if it was verified by the CA. If 'authorized' is false, a property 'authorizationError' is set on the stream.
+        if (verifyError) {
+            clearText.authorized = false;
+            clearText.authorizationError = verifyError;
+        } else {
+            clearText.authorized = true;
+        }
+        connection.stream = clearText;
+        connection.resume();
+
+        connection.callPlugins('secure', [connection]);
+    });
+
+    clearText._controlReleased = true;
+    socket.pipe(securePair.encrypted);
+    socket.write(tag+' OK Begin TLS negotiation now\r\n');
+    securePair.encrypted.pipe(socket);
+};
+
