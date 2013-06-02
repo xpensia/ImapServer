73fcd877b4f060144bc8ee0fcf4109c1036fb39b
Jean-Sébastien Tremblay <jeansebtr@xpensia.com>
2013-06-02 01:21:29 -0400

    add wrapper utility for AUTH=PLAIN

---
+++ util/auth_plain_wrapper.js
@@ -0,0 +1,28 @@
+
+var b64Stream = require('./base64_stream.js');
+
+function WrapAuthPlain(cb) {
+    function auth_plain(connection, data, next) {
+        if(data) {
+            return onData.call(this, connection, data, next);
+        }
+        var s = new b64Stream();
+        var server = this;
+        s.on('data', function(data) {
+            connection.stream.unpipe(s);
+            onData.call(server, connection, data, next);
+        });
+        connection.stream.pipe(s);
+        connection.stream.write('+ \r\n');
+    }
+    function onData(connection, data, next) {
+        var auth = data.toString('binary', 1).split('\u0000');
+        if(auth.length != 2) {
+            return next(null, 'BAD', 'Invalid credentials format.');
+        }
+        cb.call(this, connection, auth[0] || '', auth[1] || '', next);
+    }
+    return auth_plain;
+}
+module.exports = WrapAuthPlain;
+
